11343493826b7058af1666d837c740259c89ce5f
yambanso <yambanso22@gmail.com>
2026-02-10 11:51:05 +0200

    changes

--- dhis2eo/gridding/redistribution.py
+++ dhis2eo/gridding/redistribution.py
@@ -7,16 +7,20 @@ from gridding import linear_grid
 from preparedata import prepare_data
 import geopandas as gpd
 import rioxarray as rxr
+from bayersianGrid import bayesian_grid
 
-data = prepare_data(base_url="DHIS-instance-URL",username="username",password="Pwd",dx='jPEcKbn7jmh',pe="202501",ou_level="4")
+data = prepare_data(base_url="https://dhis2.health.gov.mw",username="yambansokausiwa",password="Bscinf-07",dx='jPEcKbn7jmh',pe="202501",ou_level="4") #getting data valaue to interpolate
 dataValues = pd.read_csv(StringIO(data))
-lin = linear_grid(dataValues)
-grd = mask(lin)
-
+print(dataValues)
+lin = linear_grid(dataValues) # linear interpolation
+grd = mask(lin,r"C:\Users\ShnkMn\Documents\CMS\climate-tools\docs\data\Districts.shp") #  masking takes the dataset and the path to the map layer you want to mask with
 pop = gpd.read_file(r"C:\Users\ShnkMn\Documents\CMS\climate-tools\docs\data\pop.gpkg")
 pop = pop.to_crs(epsg=4326)
 
-rst = rasterize_population(pop,lin,pop_col="population")
+overlay = gpd.read_file(r"C:\Users\ShnkMn\Documents\CMS\climate-tools\docs\data\Districts.shp")
+overlay = overlay.to_crs(epsg=4326)
+
+rst = rasterize_population(pop,lin,pop_col="population") # rasterize the population dataset
 
 rst = rst.reindex_like(lin,method=None)
 spatial_mask = lin.isel(time=0).notnull()
@@ -32,9 +36,9 @@ total_cases = lin.isel(time=0).sum(dim=("lat", "lon"))
 
 cases = weights * total_cases
 
-msk = mask(cases)
+msk = mask(cases,r"C:\Users\ShnkMn\Documents\CMS\climate-tools\docs\data\Districts.shp")
 # print(msk)
 # print(lin)
 print(msk)
-
-plotData(msk)
+plotData(grd,overlay)
+plotData(msk,overlay) # Plot data values
