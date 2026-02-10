b089e2e7b333efa17b84d5ca280a6c4f3168cd4e
yambanso <yambanso22@gmail.com>
2026-02-10 13:15:20 +0200

    bayesian gridding

--- dhis2eo/gridding/bayersianGrid.py
+++ dhis2eo/gridding/bayersianGrid.py
@@ -9,18 +9,16 @@ from rpy2 import robjects
 from rpy2.robjects import pandas2ri, default_converter
 from rpy2.robjects.conversion import localconverter
 
-def bayesian_grid(dataValues, reso=0.1, buff=0.1):
-    # 1. Prepare Grid Coordinates
+def bayesian_grid(dataValues, reso=0.0083, buff=0.0083):
     lon_min, lon_max = dataValues.lon.min(), dataValues.lon.max()
     lat_min, lat_max = dataValues.lat.min(), dataValues.lat.max()
 
     lon_grid = np.arange(lon_min - buff, lon_max + buff, reso)
     lat_grid = np.arange(lat_min - buff, lat_max + buff, reso)
 
-    # 2. Prepare Data
     dataValues = dataValues.copy()
-    # Handle missing cases
-    dataValues['cases'] = dataValues['cases'].fillna(0)
+    dataValues['cases'] = dataValues['cases'].fillna(0).astype(float)
+    dataValues['population'] = dataValues['population'].fillna(1).replace(0, 1).astype(float)
     
     dataValues["time"] = pd.PeriodIndex.from_fields(
         year=dataValues.year,
@@ -30,14 +28,12 @@ def bayesian_grid(dataValues, reso=0.1, buff=0.1):
     
     times = np.sort(dataValues["time"].unique())
 
-    # 3. Transfer to R
     with localconverter(default_converter + pandas2ri.converter):
         robjects.globalenv["df"] = robjects.conversion.py2rpy(dataValues)
 
     robjects.globalenv["lon_grid"] = robjects.FloatVector(lon_grid)
     robjects.globalenv["lat_grid"] = robjects.FloatVector(lat_grid)
 
-    # 4. Run R-INLA
     robjects.r("""
     library(INLA)
 
@@ -45,14 +41,12 @@ def bayesian_grid(dataValues, reso=0.1, buff=0.1):
       df$time <- as.factor(df$time)
       times <- levels(df$time)
 
-      # Create Mesh from coordinates
       mesh <- inla.mesh.2d(
         loc = as.matrix(df[,c("lon","lat")]),
         max.edge = c(0.2, 0.6),
         cutoff = 0.05
       )
 
-      # Define SPDE spatial model
       spde <- inla.spde2.pcmatern(
         mesh,
         prior.range = c(0.5, 0.5),
@@ -70,9 +64,8 @@ def bayesian_grid(dataValues, reso=0.1, buff=0.1):
 
         A_obs <- inla.spde.make.A(mesh, loc=as.matrix(dft[,c("lon","lat")]))
 
-        # No 'E' argument here because we aren't using population
         stk <- inla.stack(
-          data=list(y=dft$cases), 
+          data=list(y=dft$cases, E=dft$population), 
           A=list(A_obs, 1),
           effects=list(
             spatial=1:spde$n.spde,
@@ -84,17 +77,15 @@ def bayesian_grid(dataValues, reso=0.1, buff=0.1):
           y ~ 0 + intercept + f(spatial, model=spde),
           family="poisson",
           data=inla.stack.data(stk),
+          E=inla.stack.data(stk)$E, # Explicitly pass the exposure
           control.predictor=list(A=inla.stack.A(stk), compute=TRUE)
         )
 
-        # Reconstruct the field (Intercept + Spatial)
         intercept_m <- res$summary.fixed["intercept", "mean"]
         spatial_m <- res$summary.random$spatial$mean
         
-        # Projection
         field <- as.vector(intercept_m + (A_grid %*% spatial_m))
         
-        # Convert from log-link to count scale
         out[t,,] <- matrix(exp(field), nrow=length(lat_grid), byrow=TRUE)
       }
 
@@ -104,7 +95,6 @@ def bayesian_grid(dataValues, reso=0.1, buff=0.1):
     result <- run_spde(df, lon_grid, lat_grid)
     """)
 
-    # 5. Build Xarray Dataset
     data_3d = np.array(robjects.globalenv["result"])
 
     ds = xr.Dataset(
@@ -119,9 +109,9 @@ def bayesian_grid(dataValues, reso=0.1, buff=0.1):
     )
 
     ds["cases"].attrs = {
-        "long_name": "Bayesian posterior mean case density",
-        "units": "estimated cases per cell",
-        "description": "Spatial redistribution using R-INLA SPDE without population offset"
+        "long_name": "Bayesian posterior mean incidence rate",
+        "units": "cases per person",
+        "description": "Spatial redistribution using R-INLA SPDE with population offset"
     }
 
     return ds
